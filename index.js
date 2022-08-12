2323b4445682f8fa9b3c8a525cb574524de046b8
younes sheikhlar <sheikhlaryounes@mail.com>
2022-08-12 23:59:10 +0200

    creat chart 2

--- index.js
+++ index.js
@@ -64,13 +64,6 @@ window.onload = function () {
         title: {
             text: "Analytics"
         },
-        subtitles: [{
-            // text: "Click on Any Segment to Drilldown",
-            // backgroundColor: "#2eacd1",
-            fontSize: 16,
-            fontColor: "white",
-            padding: 5
-        }],
         legend: {
             fontFamily: "calibri",
             fontSize: 14,
@@ -118,5 +111,77 @@ window.onload = function () {
         chart.options.data = visitorsData["New vs Returning Visitors"];
         chart.render();
     });
-    
+
+
+}
+
+
+
+
+//          Chart 2
+
+var chart = new CanvasJS.Chart("chartContainer2", {
+    animationEnabled: true,
+   
+    axisY: {
+        title: "Billions of Barrels",
+        titleFontColor: "#4F81BC",
+        lineColor: "#4F81BC",
+        labelFontColor: "#4F81BC",
+        tickColor: "#4F81BC"
+    },
+    axisY2: {
+        title: "Millions of Barrels/day",
+        titleFontColor: "#C0504E",
+        lineColor: "#C0504E",
+        labelFontColor: "#C0504E",
+        tickColor: "#C0504E"
+    },	
+    toolTip: {
+        shared: true
+    },
+    legend: {
+        cursor:"pointer",
+        itemclick: toggleDataSeries
+    },
+    data: [{
+        type: "column",
+        name: "Proven Oil Reserves (bn)",
+        legendText: "Proven Oil Reserves",
+        showInLegend: true, 
+        dataPoints:[
+            { label: "q", y: 266.21 },
+            { label: "Venezuela", y: 302.25 },
+            { label: "Iran", y: 157.20 },
+            { label: "Iraq", y: 148.77 },
+            { label: "Kuwait", y: 101.50 },
+            { label: "UAE", y: 97.8 }
+        ]
+    },
+    {
+        type: "column",	
+        name: "Oil Production (million/day)",
+        legendText: "Oil Production",
+        axisYType: "secondary",
+        showInLegend: true,
+        dataPoints:[
+            { label: "Saudi", y: 10.46 },
+            { label: "Venezuela", y: 2.27 },
+            { label: "Iran", y: 3.99 },
+            { label: "Iraq", y: 4.45 },
+            { label: "Kuwait", y: 2.92 },
+            { label: "UAE", y: 3.1 }
+        ]
+    }]
+});
+chart.render();
+
+function toggleDataSeries(e) {
+    if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+        e.dataSeries.visible = false;
     }
+    else {
+        e.dataSeries.visible = true;
+    }
+    chart.render();
+}
